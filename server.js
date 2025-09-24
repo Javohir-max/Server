f27eec3d4892107ee1ccbd46cce2004ce9392809
Javohir <mobillepubg40@gmail.com>
2025-09-24 12:44:39 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 // app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-app.use(cors({ origin: "https://site-nu-liart.vercel.app", credentials: true }));
+// app.use(cors({ origin: "https://site-nu-liart.vercel.app", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 
 // маршруты
 app.use("/api/auth", authRoutes);
